d76f576911b0ceae1ff462cd637e7d7c2d793b4a
dkhomenko <dinamastercs@gmail.com>
2024-09-12 22:53:37 +0200

    add task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+  const inputValue = nameInput.value.trim();
+
+  nameOutput.textContent = inputValue ? inputValue : 'Anonymous';
+});
